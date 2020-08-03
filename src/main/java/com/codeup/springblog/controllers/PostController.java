ad8f86c4912ec10b59e1cd9325bb0f74fa6d9d4b
Bobbie Archambault <bmarchambault@gmail.com>
2020-08-03 15:40:01 -0500

    created controller dir

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,25 +1,62 @@
-package com.codeup.springblog;
+package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
-    @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    @ResponseBody
+    @RequestMapping(path = "/index", method = RequestMethod.GET)
+//    @ResponseBody
 //    NORMALLY THIS METHOD IS NAMED INDEX
-    public String posts(){
-        return "this is a post";
+//    public String posts(){
+//        return "this is a post";
+//    REFACTORED WITH A CORRELATING HTML THAT SHOWS ALL POSTS:
 
+    public String index(Model model){
+        List<Post>posts = new ArrayList<>();
+        Post p1 = new Post("Title1", "Some text about this post");
+        Post p2 = new Post("Title2", "Some text about this post");
+
+        posts.add(p1);
+        posts.add(p2);
+        posts.add(new Post(3, "Title 3", "some text about this post"));
+        model.addAttribute("posts", posts);
+        return "posts/index";
 }
 
-    @GetMapping ("/posts/{id}")
-    @ResponseBody
+//=========================================================================
+    @GetMapping ("/show")
+//   @ResponseBody
 //    NORMALLY THIS METHOD IS NAMED SHOW
-    public String postsId(@PathVariable long id) {
-        return "this is a post id: " + id;
+//    public String postsId(@PathVariable long id) {
+//       return "this is a post id: " + id;
+//       REFACTORED WITH A CORRELATING HTML THAT SHOW ONE POST:
+        public String show( Model model) {
+        Post p1 = new Post( "TitleUno", "Some text about this post");
+
+        model.addAttribute("postTitle", p1.getTitle());
+        model.addAttribute("postBody", p1.getBody());
+            return "posts/show";
     }
+    //**********************************************************************
+//INSTRUCTOR SOLUTION BELOW.  THE ABOVE GETMAPPING /SHOW WORKS TOO. :
+
+    @GetMapping ("/post/{id}")
+    public String show( @PathVariable int id, Model model) {
+        Post myPost = new Post( "SophiesPost", "Hello World");
+
+        model.addAttribute("title", myPost.getTitle());
+        model.addAttribute("body", myPost.getBody());
+        return "posts/show";
+    }
+
+//    ====================================================================
 
     @GetMapping ("/posts/create")
     @ResponseBody
--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -1,4 +1,54 @@
-package models;
+package com.codeup.springblog.models;
 
 public class Post {
+    private int id;
+    private String title;
+    private String body;
+
+
+ public Post (){};
+
+ public Post(String title, String body){
+    this.title = title;
+    this.body = body;
+ }
+
+    public Post(int id, String title, String body){
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
 }
