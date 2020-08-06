642cc44997626f4ab07f9ceb0b575e9474d22036
Bobbie Archambault <bmarchambault@gmail.com>
2020-08-06 09:45:19 -0500

    created a user respository

--- src/main/java/com/codeup/springblog/repositories/UserRepository.java
+++ src/main/java/com/codeup/springblog/repositories/UserRepository.java
@@ -1,2 +1,7 @@
-package com.codeup.springblog.repositories;public class UserRepository {
+package com.codeup.springblog.repositories;
+
+import com.codeup.springblog.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
