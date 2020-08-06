49518fae9881663cc4299261ea2cd438cb688cbe
Bobbie Archambault <bmarchambault@gmail.com>
2020-08-06 09:42:16 -0500

    created mock data

--- src/main/java/com/codeup/springblog/models/Dog.java
+++ src/main/java/com/codeup/springblog/models/Dog.java
@@ -1,4 +1,25 @@
 package com.codeup.springblog.models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="dogs")
 public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 3, columnDefinition = "TINYINT(3) UNSIGNED")
+    private int age;
+
+    @Column(nullable = false, length = 200)
+    private String name;
+
+    @Column(length = 2, columnDefinition = "CHAR(2) DEFAULT'XX'")
+    private String reside_state;
 }
+
+
+
+
