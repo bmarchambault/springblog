8a0a711473d038c339f2db564288b7c924729798
Bobbie Archambault <bmarchambault@gmail.com>
2020-08-06 16:12:11 -0500

    refactored code

--- src/main/java/com/codeup/springblog/models/Ad.java
+++ src/main/java/com/codeup/springblog/models/Ad.java
@@ -1,17 +1,70 @@
 package com.codeup.springblog.models;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "ads")
 public class Ad {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-@Column(length = 50, nullable = false, unique = true)
+
+    @Column(length = 50, nullable = false, unique = true)
     private String title;
 
-@Column(columnDefinition = "TEXT NOT NULL")
+    @Column(columnDefinition = "TEXT NOT NULL")
     private String description;
 
+    @OneToMany(mappedBy = "parentAd")
+    @JsonManagedReference
+    private List<Comment> comments;
+
+    public Ad() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Ad{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
