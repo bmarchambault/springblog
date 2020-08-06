6a657503b670c8ecfc0536f747ef16ad24cc6aa2
Bobbie Archambault <bmarchambault@gmail.com>
2020-08-06 09:47:49 -0500

    completed mini exercise

--- src/main/java/com/codeup/springblog/models/Dog.java
+++ src/main/java/com/codeup/springblog/models/Dog.java
@@ -1,24 +1,24 @@
-package com.codeup.springblog.models;
-
-
-import javax.persistence.*;
-
-@Entity
-@Table(name="dogs")
-public class Dog {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
-    @Column(nullable = false, length = 3, columnDefinition = "TINYINT(3) UNSIGNED")
-    private int age;
-
-    @Column(nullable = false, length = 200)
-    private String name;
-
-    @Column(length = 2, columnDefinition = "CHAR(2) DEFAULT'XX'")
-    private String reside_state;
-}
+//package com.codeup.springblog.models;
+//
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name="dogs")
+//public class Dog {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private long id;
+//
+//    @Column(nullable = false, length = 3, columnDefinition = "TINYINT(3) UNSIGNED")
+//    private int age;
+//
+//    @Column(nullable = false, length = 200)
+//    private String name;
+//
+//    @Column(length = 2, columnDefinition = "CHAR(2) DEFAULT'XX'")
+//    private String reside_state;
+//}
 
 
 
