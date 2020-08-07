1b6d57c3c8b129aa9ccdc6f2e2c9cf79b5eccbff
Bobbie Archambault <bmarchambault@gmail.com>
2020-08-07 15:50:27 -0500

    created geeeting svc

--- src/main/java/com/codeup/springblog/services/GreetingSvc.java
+++ src/main/java/com/codeup/springblog/services/GreetingSvc.java
@@ -1,2 +1,21 @@
-package com.codeup.springblog.services;public class GreetingSvc {
+package com.codeup.springblog.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingSvc {
+
+    public String goodMorning(String name){
+        return"Good Morning" + name + "!";
+
+    }
+    public String goodAfternoon(String name){
+        return"Good Afternoon" + name + "!";
+
+    }
+    public String goodEvening(String name){
+        return"Good Evening" + name + "!";
+
+    }
+
 }
