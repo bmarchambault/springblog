8362906a3b98628ed02222a6dee4371fc07d31e3
Bobbie Archambault <bmarchambault@gmail.com>
2020-08-07 15:49:15 -0500

    refactored

--- src/main/java/com/codeup/springblog/controllers/AdController.java
+++ src/main/java/com/codeup/springblog/controllers/AdController.java
@@ -4,9 +4,7 @@ import com.codeup.springblog.models.Ad;
 import com.codeup.springblog.repositories.AdRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -57,5 +55,18 @@ public class AdController {
     public String getTestAd() {
         return adsDao.findByTitle("title 1").toString();
     }
+
+    @GetMapping("/ads/create")
+    public String showCreateForm(Model model){
+        model.addAttribute("ad", new Ad());
+        return "ads/create";
+    }
+
+    @PostMapping("/ads/create")
+    public String createAd(@ModelAttribute Ad ad){
+        adsDao.save(ad);
+        return "redirect: ads/view";
+    }
+
 
 }
