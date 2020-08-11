cba84037774cf8bc0e5329b3d9d8d32a4d5c585a
Bobbie Archambault <bmarchambault@gmail.com>
2020-08-11 14:50:22 -0500

    created securty config file

---
+++ src/main/java/com/codeup/springblog/SecurityConfiguration.java
@@ -0,0 +1,73 @@
+package com.codeup.springblog;
+
+import com.codeup.springblog.services.UserDetailsLoader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+// ************************************************************************************************
+// configuration class determines:
+//Which pages will require authentication
+//Which pages are available to anyone
+//The path to the login page
+//The hashing algorithm we'll use to store passwords
+//***************************************************************************************************
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsLoader usersLoader;
+
+    public SecurityConfiguration(UserDetailsLoader usersLoader) {
+        this.usersLoader = usersLoader;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(usersLoader) // How to find users by their username
+                .passwordEncoder(passwordEncoder()) // How to encode and verify passwords
+        ;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                /* Login configuration */
+                .formLogin()
+                .loginPage("/login")
+//                .defaultSuccessUrl("/ads") // user's home page, it can be any URL
+                .defaultSuccessUrl("/posts") // user's home page, it can be any URL
+                .permitAll() // Anyone can go to the login page
+                /* Logout configuration */
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login?logout") // append a query string value
+                /* Pages that can be viewed without having to log in */
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/ads", "/posts", "/sign-up", "/login") // anyone can see the home and the ads pages
+                .permitAll()
+                /* Pages that require authentication */
+                .and()
+                .authorizeRequests()
+//  COMMA SEPARATE PAGES WHICH REQUIRE AUTHENTICATION:
+                .antMatchers(
+                        "/ads/create", // only authenticated users can create ads
+                        "/ads/{id}/edit" , // only authenticated users can edit ads
+                        "/posts/create",
+                        "/posts/{id}/edit",
+                        "/posts/{id}/delete"
+                )
+                .authenticated()
+        ;
+    }
+}
